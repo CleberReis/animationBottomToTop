a0c43e8d30b63d77514b380aa5cfe65b3c328ec5
Cleber Reis <cleberreisdeveloper@gmail.com>
2019-10-29 11:15:34 -0300

    projeto animação

---
+++ animationBottomToTop/Controllers/CardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CardViewController.swift
+//  animationBottomToTop
+//
+//  Created by Cleber Reis on 28/10/19.
+//  Copyright © 2019 Cleber Reis. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+    
+    @IBOutlet weak var handleArea: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
